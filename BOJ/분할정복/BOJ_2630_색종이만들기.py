2bf6cae9ad4301c8c61fd06a6dde5d8c29233734
sirzzang <sirzzang@naver.com>
2020-12-19 23:40:23 +0900

    20201219 update2

--- BOJ/분할정복/BOJ_2630_색종이만들기.py
+++ BOJ/분할정복/BOJ_2630_색종이만들기.py
@@ -1,27 +1,25 @@
-# n = int(input())
-# squares = [''.join(input().split()) for _ in range(n)]
-# print(squares)
-
-# 하나로 이루어져 있는지 체크
+answer = [0, 0]
 
 def solution(squares_arr):
     squares = ''.join(squares_arr)
     if squares == len(squares)*squares[0]:
-        print('탈출', squares[0])
+        answer[int(squares[0])] += 1
         return
 
+    half = len(squares_arr) // 2
     sub_squares = []
     for row in range(2):
-        part = squares_arr[row*(n//2):(row+1)*(n//2)]
+        partition = squares_arr[row*half : (row+1)*half] # row로 partition
         for col in range(2):
-            sub_squares.append([p[col*(n//2):(col+1)*(n//2)] for p in part])
+            sub_squares.append([p[col*half : (col+1)*half] for p in partition])
 
     for i in range(4):
-        print(sub_squares[i])
+        solution(sub_squares[i])
 
-
-# 4개로 나누기
 if __name__ == '__main__':
     n = int(input())
     squares = ['11000011', '11000011', '00001100', '00001100', '10001111', '01001111', '00111111', '00111111']
+    # squares = [''.join(input().split()) for _ in range(n)]
     solution(squares)
+    print(answer[0])
+    print(answer[1])
