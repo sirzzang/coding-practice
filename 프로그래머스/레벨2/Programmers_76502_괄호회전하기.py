8d26abee01d0eea097d36dd5bc8d9254da3cbce3
sirzzang <sirzzang@naver.com>
2021-05-04 10:51:17 +0900

    20210504 programmers 괄호 회전 풀이2: 괄호 개수 확인 추가

--- 프로그래머스/레벨2/Programmers_76502_괄호회전하기.py
+++ 프로그래머스/레벨2/Programmers_76502_괄호회전하기.py
@@ -1,21 +1,22 @@
-# 풀이 1
+# 풀이 2
 # - 괄호 검사 시 전부 분기처리 괜찮은가?
 # - 회전도 O(N)인데?
+# - 괄호 검사 시 solution에서 괄호 개수 맞는지 확인했더니 통과
 '''
 정확성  테스트
-테스트 1 〉	통과 (7.12ms, 10.2MB)
-테스트 2 〉	통과 (3.97ms, 10.3MB)
-테스트 3 〉	통과 (4.19ms, 10.2MB)
-테스트 4 〉	통과 (5.09ms, 10.2MB)
-테스트 5 〉	통과 (19.30ms, 10.2MB)
-테스트 6 〉	통과 (7.98ms, 10.3MB)
-테스트 7 〉	통과 (10.06ms, 10.2MB)
-테스트 8 〉	통과 (14.12ms, 10.3MB)
-테스트 9 〉	통과 (27.20ms, 10.2MB)
-테스트 10 〉	통과 (38.84ms, 10.2MB)
-테스트 11 〉	통과 (61.96ms, 10.2MB)
-테스트 12 〉	통과 (0.01ms, 10.2MB)
-테스트 13 〉	실패 (0.01ms, 10.2MB)
+테스트 1 〉	통과 (7.14ms, 10.2MB)
+테스트 2 〉	통과 (4.27ms, 10.2MB)
+테스트 3 〉	통과 (6.13ms, 10.2MB)
+테스트 4 〉	통과 (5.34ms, 10.2MB)
+테스트 5 〉	통과 (18.71ms, 10.3MB)
+테스트 6 〉	통과 (7.22ms, 10.2MB)
+테스트 7 〉	통과 (10.67ms, 10.2MB)
+테스트 8 〉	통과 (13.58ms, 10.2MB)
+테스트 9 〉	통과 (26.75ms, 10.2MB)
+테스트 10 〉	통과 (37.10ms, 10.2MB)
+테스트 11 〉	통과 (57.03ms, 10.3MB)
+테스트 12 〉	통과 (0.00ms, 10.2MB)
+테스트 13 〉	통과 (0.00ms, 10.2MB)
 테스트 14 〉	통과 (0.01ms, 10.2MB)
 '''
 
@@ -51,6 +52,10 @@ def check_parentheses(brackets):
 
 def solution(s):
 
+    if s.count('(') + s.count('{') + s.count('[') !=\
+            s.count(')')+s.count('}')+s.count(']'):
+        return 0
+
     answer = 0
     s_len = len(s)
 
@@ -58,6 +63,5 @@ def solution(s):
     for i in range(s_len):
         rotate = s[i:s_len] + s[0:i]
         answer += check_parentheses(rotate)
-        # print(check_parentheses(rotate))
 
     return answer
