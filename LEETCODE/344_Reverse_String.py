ab596d4c0afff95e9ab46b1b584b2bd5aecdaf86
sirzzang <sirzzang@naver.com>
2021-06-24 13:11:46 +0900

    20210624 LeetCode 937

--- LEETCODE/344_Reverse_String.py
+++ LEETCODE/344_Reverse_String.py
@@ -1,10 +1,12 @@
 from typing import List
 
+# Follow up: Do not allocate extra space for another array. 
+# You must do this by modifying the input array in-place with O(1) extra memory.
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s.reverse()
 
-def reverseString(s: List[str]) -> None:
-    print(s)
-    s.reverse()
-    print(s)
 
-
-reverseString(["h", "e", "l", "l", "o"])
