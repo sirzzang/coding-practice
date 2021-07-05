84e58c3f9095db6594422e018e4bb48e0c072314
sirzzang <sirzzang@naver.com>
2021-07-05 10:13:06 +0900

    20210705 LeetCode 42

--- LEETCODE/42_Trapping_Rain_Water.py
+++ LEETCODE/42_Trapping_Rain_Water.py
@@ -1,29 +1,61 @@
 from typing import List, Tuple
 
-
-def split(height: List[int]) -> Tuple[List[int], List[int]]:
-    max_height = max(height)
-    max_idx = height.index(max_height)
-    return height[:max_idx+1], height[max_idx:][::-1]
-
-
-def collect(height: List[int]) -> int:
-    wall = 0
-    water = 0
-    for h in height:
-        if h > wall:
-            wall = h
-        water += wall-h
-    return water
-
-
-def trap(height: List[int]) -> int:
-    l_height, r_height = split(height)
-    print(collect(l_height))
-    print(collect(r_height))
+# runtime error
+'''
+TypeError: collect() missing 1 required positional argument: 'height'
     return collect(l_height) + collect(r_height)
+Line 31 in trap (Solution.py)
+    ret = Solution().trap(param_1)
+Line 52 in _driver (Solution.py)
+    _driver()
+Line 63 in <module> (Solution.py)
+'''
 
 
-# l, r = split([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1, 4, 2, 1, 0, 2, 1, 0, 1])
-trap([4, 2, 0, 3, 2, 5])
-trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1, 4, 2, 1, 0, 2, 1, 0, 1])
+class Solution:
+    def split(self, height: List[int]) -> Tuple[List[int], List[int]]:
+        max_height = max(height)
+        max_idx = height.index(max_height)
+        return height[:max_idx+1], height[max_idx:][::-1]
+
+    def collect(self, height: List[int]) -> int:
+        wall = 0
+        water = 0
+        for h in height:
+            if h > wall:
+                wall = h
+            water += wall-h
+        return water
+
+    def trap(self, height: List[int]) -> int:
+        l_height, r_height = split(height)
+        return collect(l_height) + collect(r_height)
+
+
+# accepted
+'''
+Runtime: 48 ms
+Memory Usage: 14.9 MB
+'''
+
+
+class Solution:
+
+    def trap(self, height: List[int]) -> int:
+        if not height:
+            return 0
+
+        max_height = max(height)
+        max_idx = height.index(max_height)
+        l_height, r_height = height[:max_idx+1], height[max_idx:][::-1]
+
+        def collect(height: List[int]) -> int:
+            wall = 0
+            water = 0
+            for h in height:
+                if h > wall:
+                    wall = h
+                water += wall-h
+            return water
+
+        return collect(l_height) + collect(r_height)
