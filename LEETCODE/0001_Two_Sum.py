15701d5f57c81de8e64708dc2fd35a65c7ab9366
sirzzang <sirzzang@naver.com>
2021-09-06 00:37:01 +0900

    프로그래머스 위클리챌린지 5주차

--- LEETCODE/0001_Two_Sum.py
+++ LEETCODE/0001_Two_Sum.py
@@ -13,13 +13,20 @@ from typing import List
 
 
 # 2. 인덱스 유지하기 위해 원래 자리에 더미 원소 추가: 비효율적
+# def twoSum(nums: List[int], target: int) -> List[int]:
+#     for i, v in enumerate(nums):
+#         temp = nums[:i] + [''] + nums[i+1:]  # 인덱스 유지
+#         if target - v in temp:
+#             return [i, temp.index(target-v)]
+
 def twoSum(nums: List[int], target: int) -> List[int]:
+    print()
     for i, v in enumerate(nums):
-        temp = nums[:i] + [''] + nums[i+1:]  # 인덱스 유지
-        if target - v in temp:
-            return [i, temp.index(target-v)]
+        if v > target:
+            continue
+        if target - v in nums:
+            print(i, v, nums.index(target-v))
 
-
-print(twoSum(nums=[3, 2, 4], target=6))
-print(twoSum(nums=[2, 7, 11, 15], target=9))
-print(twoSum([3, 2, 3], 6))
+twoSum([3, 2, 4], 6)
+twoSum([2, 7, 11, 15], 9)
+twoSum([3, 3], 6)
