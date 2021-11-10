3656d5bafa026c2a609779fde9bd53145507514c
sirzzang <sirzzang@naver.com>
2021-11-10 11:20:21 +0900

    week03 네트워크

--- week03_네트워크.py
+++ week03_네트워크.py
@@ -1,6 +1,7 @@
 def dfs(node, computers, visited):
     visited[node] = True
     for i, v in enumerate(computers[node]):
+        # 연결되어 있는 컴퓨터의 경우 다시 탐색
         if v and i != node and not visited[i]:
             dfs(i, computers, visited)
 
@@ -11,6 +12,7 @@ def solution(n, computers):
     for node in range(n):
         if not visited[node]:
             dfs(node, computers, visited)
+            # 탐색 마친 경우 네트워크 추가
             answer += 1
     
     return answer
