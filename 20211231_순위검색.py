c71c63b166b70937392ea34dd314d435f28e8f01
sirzzang <sirzzang@naver.com>
2021-12-31 16:42:27 +0900

    week10 순위 검색 정답 풀이 추가

--- 20211231_순위검색.py
+++ 20211231_순위검색.py
@@ -1,3 +1,68 @@
+# 1. info 조합 구한 뒤 query에서 검색
+# 통과
+'''
+정확성  테스트
+테스트 1 〉	통과 (0.25ms, 10.4MB)
+테스트 2 〉	통과 (0.24ms, 10.4MB)
+테스트 3 〉	통과 (0.29ms, 10.4MB)
+테스트 4 〉	통과 (1.44ms, 10.5MB)
+테스트 5 〉	통과 (3.77ms, 10.5MB)
+테스트 6 〉	통과 (3.70ms, 10.5MB)
+테스트 7 〉	통과 (3.06ms, 10.8MB)
+테스트 8 〉	통과 (31.02ms, 11.5MB)
+테스트 9 〉	통과 (34.14ms, 11.6MB)
+테스트 10 〉	통과 (32.90ms, 11.8MB)
+테스트 11 〉	통과 (1.97ms, 10.6MB)
+테스트 12 〉	통과 (3.77ms, 10.6MB)
+테스트 13 〉	통과 (3.00ms, 10.7MB)
+테스트 14 〉	통과 (16.94ms, 11MB)
+테스트 15 〉	통과 (15.73ms, 11MB)
+테스트 16 〉	통과 (1.93ms, 10.5MB)
+테스트 17 〉	통과 (3.70ms, 10.6MB)
+테스트 18 〉	통과 (15.70ms, 10.9MB)
+효율성  테스트
+테스트 1 〉	통과 (519.70ms, 42.8MB)
+테스트 2 〉	통과 (533.00ms, 42.7MB)
+테스트 3 〉	통과 (511.10ms, 42.5MB)
+테스트 4 〉	통과 (513.81ms, 42.6MB)
+'''
+from itertools import combinations
+from bisect import bisect_left
+
+def solution(info, query):
+    answer = []
+    info_dict = {}
+    for i in info:
+        candidate = i.split()
+        spec, score = candidate[:-1], int(candidate[-1])
+        for n in range(5):
+            for comb in combinations(spec, n):
+                k = ''.join(comb)
+                if k not in info_dict:
+                    info_dict[k] = [score]
+                else:
+                    info_dict[k].append(score)        
+    
+    for v in info_dict.values():
+        v.sort()
+    
+    for q in query:
+        q = [elem for elem in q.split() if elem not in ['and', '-']]
+        q_spec, q_score = ''.join(q[:-1]), int(q[-1])
+        if q_spec in info_dict:
+            score_list = info_dict[q_spec]
+            idx = bisect_left(score_list, q_score)
+            answer.append(len(score_list) - idx)
+        else:
+            answer.append(0)
+
+    return answer
+
+# 시간 초과
+
+
+# 2. info_dict, score_list로 재구성한 뒤, 각 query 검색
+# 시간 초과
 '''
 정확성  테스트
 테스트 1 〉	통과 (0.09ms, 10.5MB)
